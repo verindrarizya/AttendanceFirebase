d6da41cd15a81ee72895eaa33c3e554aa7d530ff
verindrarizya <v.rizya22@gmail.com>
2023-07-24 12:20:56 +0700

    remove navigate to onboarding option

--- app/src/main/java/com/verindrarizya/attendancefirebase/ui/screens/onboarding/OnBoardingNavigation.kt
+++ app/src/main/java/com/verindrarizya/attendancefirebase/ui/screens/onboarding/OnBoardingNavigation.kt
@@ -1,20 +1,11 @@
 package com.verindrarizya.attendancefirebase.ui.screens.onboarding
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.navOptions
 import com.verindrarizya.attendancefirebase.ui.screens.Destination
 
 object OnBoardingDestination : Destination {
     override val routeName: String = "OnBoardingDestination"
-}
-
-fun NavController.navigateToOnBoarding(
-    builder: NavOptionsBuilder.() -> Unit = {}
-) {
-    this.navigate(OnBoardingDestination.routeName, navOptions(builder))
 }
 
 fun NavGraphBuilder.onBoardingScreen(
