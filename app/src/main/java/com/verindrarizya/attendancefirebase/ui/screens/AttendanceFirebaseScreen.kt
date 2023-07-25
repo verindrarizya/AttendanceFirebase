c577572892b26c09f6b4a92c645a5093785d8ad3
verindrarizya <v.rizya22@gmail.com>
2023-07-25 09:52:52 +0700

    adjustment on how nested graph should handle navigation

--- app/src/main/java/com/verindrarizya/attendancefirebase/ui/screens/AttendanceFirebaseScreen.kt
+++ app/src/main/java/com/verindrarizya/attendancefirebase/ui/screens/AttendanceFirebaseScreen.kt
@@ -10,7 +10,6 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
@@ -21,13 +20,10 @@ import com.verindrarizya.attendancefirebase.ui.navigation.GlobalAuthDestination
 import com.verindrarizya.attendancefirebase.ui.navigation.authGraph
 import com.verindrarizya.attendancefirebase.ui.navigation.navigateToGlobalAuth
 import com.verindrarizya.attendancefirebase.ui.navigation.popUpToInclusive
-import com.verindrarizya.attendancefirebase.ui.screens.authentication.login.LoginDestination
 import com.verindrarizya.attendancefirebase.ui.screens.authentication.login.navigateToLogin
-import com.verindrarizya.attendancefirebase.ui.screens.authentication.register.RegisterDestination
 import com.verindrarizya.attendancefirebase.ui.screens.authentication.register.navigateToRegister
 import com.verindrarizya.attendancefirebase.ui.screens.dashboard.DashboardDestination
 import com.verindrarizya.attendancefirebase.ui.screens.dashboard.dashboardScreen
-import com.verindrarizya.attendancefirebase.ui.screens.dashboard.navigateToDashboard
 import com.verindrarizya.attendancefirebase.ui.screens.onboarding.OnBoardingDestination
 import com.verindrarizya.attendancefirebase.ui.screens.onboarding.onBoardingScreen
 
@@ -38,7 +34,6 @@ fun AttendanceFirebaseScreen(
     viewModel: AttendanceFirebaseViewModel = viewModel()
 ) {
     val isUserOnBoarded: Boolean? by viewModel.isUserAlreadyOnBoarded.collectAsStateWithLifecycle()
-    val currentBackState by navController.currentBackStackEntryAsState()
 
     if (isUserOnBoarded != null) {
         NavHost(
@@ -64,41 +59,7 @@ fun AttendanceFirebaseScreen(
                     }
                 }
             )
-            authGraph(
-                onNavigateToDashboardScreen = {
-                    navController.navigateToDashboard {
-                        popUpToInclusive(
-                            if (currentBackState?.destination?.route == RegisterDestination.routeName) {
-                                RegisterDestination
-                            } else {
-                                LoginDestination
-                            }
-                        )
-                    }
-                },
-                onNavigateToLoginScreen = {
-                    navController.navigateToLogin {
-                        popUpToInclusive(
-                            if (currentBackState?.destination?.route == RegisterDestination.routeName) {
-                                RegisterDestination
-                            } else {
-                                LoginDestination
-                            }
-                        )
-                    }
-                },
-                onNavigateToRegisterScreen = {
-                    navController.navigateToRegister {
-                        popUpToInclusive(
-                            if (currentBackState?.destination?.route == RegisterDestination.routeName) {
-                                RegisterDestination
-                            } else {
-                                LoginDestination
-                            }
-                        )
-                    }
-                }
-            )
+            authGraph(navController = navController)
             dashboardScreen(
                 onNavigateToAuth = {
                     navController.navigateToGlobalAuth {
